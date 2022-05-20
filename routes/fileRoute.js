1ff9154b9ab08eb26cdfb999adc6779610f3cfbd
Abhi021-coder <abhishekSharmaas015@gmail.com>
2022-05-20 18:55:09 +0530

    hidding secret data

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./vars/.env" });
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
